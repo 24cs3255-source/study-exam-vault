80174bb2f0404a95ceac26bda2eb2aba61e9d314
24cs3255-source <24cs3255@gmail.com>
2026-07-19 11:12:30 +0530

    Add files via upload

--- index.py
+++ index.py
@@ -1,7 +1,6 @@
 import os
 import sys
 
-# Add parent directory to path so app can be imported when run as a serverless function
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app import app
